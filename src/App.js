982e51fceaaf56c2506990f68c6c45578c23a829
samikshajain360 <samikshajain235@gmail.com>
2026-06-14 13:10:37 +0530

    add dividers

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import logoFolioPage from "./Frame-188.png";
 import brandIdentityPage from "./Desktop-1.png";
 import bunnyPage from "./Desktop-2.png";
 import myWheelsPage from "./Desktop-3.png";
+import orangedivider from "./orangedivider.png";
+import Social from "./Desktop-4.png";
+import SocialDivider from "./Frame-65.png";
+import logofolioDivider from "./Group-5.png";
+import bunnydivider from "./bunnydivider.png";
 import socialMediaPage from "./Desktop-5.png";
 
 function App() {
@@ -13,10 +18,27 @@ function App() {
         src={portfolioPage}
         alt="Portfolio page"
       />
+
+      <img
+        className="block w-full h-auto"
+        src={logofolioDivider}
+        alt="Portfolio page"
+      />
       <img
         className="block w-full h-auto"
         src={logoFolioPage}
         alt="Logo folio page"
+      />
+      
+      <img
+        className="block w-full h-auto"
+        src={logoFolioPage}
+        alt="Logo folio page"
+      />
+            <img
+        className="block w-full h-auto"
+        src={orangedivider}
+        alt="Portfolio page"
       />
       <img
         className="block w-full h-auto"
@@ -30,9 +52,25 @@ function App() {
       />
       <img
         className="block w-full h-auto"
+        src={bunnydivider}
+        alt="Bunny brand identity page"
+      />
+      <img
+        className="block w-full h-auto"
         src={myWheelsPage}
         alt="My Wheels brand identity page"
       />
+      <img
+        className="block w-full h-auto"
+        src={Social}
+        alt="My Wheels brand identity page"
+      />
+        <img
+        className="block w-full h-auto"
+        src={SocialDivider}
+        alt="Decorative divider between My Wheels and social media"
+      />
+    
       <img
         className="block w-full h-auto"
         src={socialMediaPage}
